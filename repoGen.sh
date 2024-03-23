8d695f5698dcabf2638e70f928d1f9495f867236
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-03-23 16:55:11 -0400

    Cut: Removed Vite Options
    Extra script that installs JS and TS Deps for Vite will be used instead of adding it to repoGen.sh

--- repoGen.sh
+++ repoGen.sh
@@ -38,7 +38,7 @@ webpackJSDependencies(){
     cp $HOME/repoGen/javascript/webpack.production.js webpack.production.js 
     cp $HOME/repoGen/javascript/webpack.dev.js webpack.dev.js 
     cp $HOME/repoGen/javascript/webpack.common.js webpack.common.js 
-    ## Create Source folder and templates, css, and ts folders 
+    ## Create Source folder and templates, css, and js folders 
     mkdir src
     cd src
     mkdir templates js css
@@ -53,6 +53,7 @@ webpackJSDependencies(){
     cd ../../
 }
 
+# Install and Create all dependencies and starter files for Webpack TS repo
 webpackTSDependencies(){
     ## initalize git repo and install eslint, prettier, webpack and typescript
     pnpm add -D prettier eslint-config-prettier  \
@@ -69,97 +70,61 @@ webpackTSDependencies(){
     cp $HOME/repoGen/typescript/webpack.production.js webpack.production.js 
     cp $HOME/repoGen/typescript/webpack.dev.js webpack.dev.js 
     cp $HOME/repoGen/typescript/webpack.common.js webpack.common.js 
-}
 
-viteJS(){
-    ## Run Create Vite command with param as repo title
-    pnpm create vite $i --template vanilla
-    cd $i
-
-    ## Copy Favicon, gitignore and prettier config 
-    cp $HOME/repoGen/favicon.ico favicon.ico
-    cp $HOME/repoGen/.prettierrc.toml .prettierrc.toml
-    cp $HOME/repoGen/javascript/.eslintrc.json .eslintrc.json
-    cp $HOME/repoGen/vite.config.js vite.config.js 
-    cp $HOME/repoGen/readme_template.md README.md
-
-    ## install eslint and prettier
-    pnpm install -D \
-      prettier \
-      eslint-config-prettier \
-      eslint \
-      eslint-plugin-prettier \
-      vite-plugin-eslint \
-      vitest
-
-    sed -i '8s/$/,/' package.json
-    sed -i '8a "test":"vitest"' package.json
-}
-
-viteTS(){
-    ## Run Create Vite command with param as repo title
-    pnpm create vite $i --template vanilla
-    cd $i
-
-    ## Copy Favicon, gitignore and prettier config 
-    cp $HOME/repoGen/favicon.ico favicon.ico
-    cp $HOME/repoGen/.prettierrc.toml .prettierrc.toml
-    cp $HOME/repoGen/javascript/.eslintrc.json .eslintrc.json
-    cp $HOME/repoGen/vite.config.js vite.config.js 
-    cp $HOME/repoGen/readme_template.md README.md
-
-    ## install eslint and prettier
-    pnpm install -D \
-      prettier \
-      eslint-config-prettier \
-      eslint \
-      eslint-plugin-prettier \
-      vite-plugin-eslint \
-      vitest
-
-    sed -i '8s/$/,/' package.json
-    sed -i '8a "test":"vitest"' package.json
+    ## Create Source folder and templates, css, and ts folders 
+    mkdir src
+    cd src
+    mkdir templates ts css
+    cd templates 
+    cp $HOME/repoGen/index.html index.html
+    cd ../
+    cd ts
+    cp $HOME/repoGen/index.ts index.ts
+    cd ../
+    cd css
+    cp $HOME/repoGen/styles.css styles.css 
+    cd ../../
 }
 
 help(){
-    echo "Help" >&2
     echo -e "\033[32m ----------------------------------------\033[0m"
     echo -e "\033[32m Help: Genreating Webpack Repo "
-    echo -e "\033[32m [-w] Generate Webpack Repo "
-    echo -e "\033[32m [-j] Add Javascript Dependencies "
-    echo -e "\033[32m [-t] Add Typescript Dependencies "
-    echo -e "\033[32m ----------------------------------------\033[0m"
-    echo -e "\033[32m ----------------------------------------\033[0m"
-    echo -e "\033[32m Help: Genreating Vite Repo "
-    echo -e "\033[32m [-v] Generate Webpack Repo "
+    echo -e "\033[32m Example: ./repoGen.sh -j repo-name "
     echo -e "\033[32m [-j] Add Javascript Dependencies "
     echo -e "\033[32m [-t] Add Typescript Dependencies "
     echo -e "\033[32m ----------------------------------------\033[0m"
     exit 1
 }
 
-if [[ $1 == "-h" ]]; then
- help 
-fi
-if [[ $1 == "-w" && $2 == "-j" && ! -z $3 ]]; then
- mkdir $3 
- cd $3
- webpack
- webpackJSDependencies
-fi
-if [[ $1 == "-w" && $2 == "-t" && ! -z $3 ]]; then
- mkdir $3 
- cd $3
- webpack
- webpackTSDependencies 
-fi
-if [[ $1 == "-v" && $2 == "-j" && ! -z $3 ]]; then
- mkdir $3 
- cd $3
- viteJS 
-fi
-if [[ $1 == "-v" && $2 == "-t" && ! -z $3 ]]; then
-mkdir $3 
-cd $3
-viteTS 
-fi
+while getopts "j:t:h" opt; do 
+  case $opt in
+    j)
+      i="$OPTARG"
+      echo "$repoGenTitle"
+
+      mkdir $i
+
+      cd $i
+
+      webpack
+
+      webpackJSDependencies
+      ;;
+    t)
+      i="$OPTARG"
+      echo "$repoGenTitle"
+
+      mkdir $i
+
+      cd $i
+
+      webpack
+
+      webpackTSDependencies
+      ;;
+    h)
+      help  
+      ;;
+    esac
+  done
+  shift $((OPTIND -1))
